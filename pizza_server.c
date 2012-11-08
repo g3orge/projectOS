189a9e5ba5a5ba2cc0c8baf51a44601250cca727
George Papanikolaou <g3orge.app@gmail.com>
2012-11-09 00:30:17 +0200

    Starting to implement "real" semaphores and...
    -Cook function
    -Delivery function
    -Basic semaphore stuff
    -Picking the right cooker (for the first 10)
    -Fixes (to use N_PSISTES etc...)
    -Also removed the ec_malloc function
    
    --TODO:
    make fatal work with a file (log) so we can see what the childs are
    saying and split some declarations to a header file

--- pizza_server.c
+++ pizza_server.c
@@ -46,7 +46,7 @@ typedef enum { false, true } bool;
 
 /* Struct for the pizza order */
 typedef struct {
-    int m_num;
+    short m_num;
     short p_num;
     short s_num;
     bool time;
@@ -54,32 +54,41 @@ typedef struct {
     bool status2;
 } order_t;
 
-/* A function to display an error message and then exit */
 void fatal(char *message) {
+    /* A function to display an error message and then exit */
+    /* Maybe put it in a logfile so it can be accessible from everywhere */
     fprintf(stderr, "\a!! - Fatal error - ");
     fprintf(stderr, "%s\n", message);
     exit(EXIT_FAILURE);
 }
 
-/* An error checked malloc() wrapper function */
-void *ec_malloc(unsigned int size) {
-    void *ptr;
-    ptr = malloc(size);
-    if(ptr == NULL)
-        fatal("in ec_malloc() on memory allocation");
-    return ptr;
-}
-
 void handler(int sig_num) {
-    /* handling the Signal of the client SIGUSR1 (10) */
+    /* A simple handler maybe for stats */
+    fprintf(stderr,"\nCathing Terminate Signal");
+    exit(EXIT_SUCCESS);
 }
 
-void cook() {
-    /* Cooker function */
+void cook(char t) {
+    /* The waiting function for the cooking */
+    if (t == 'm')
+        usleep(TIME_MARGARITA);
+    else if (t == 'p')
+        usleep(TIME_PEPPERONI);
+    else if (t == 's')
+        usleep(TIME_SPECIAL);
+    else
+        fatal("Wrong input on cook function");
+    /* The cook functions does not change the statuses of the orders */
 }
 
-void deliverer() {
-    /* Delivery function */
+void deliverer(char d) {
+    /* The waiting function for the delivery */
+    if (d == 's')
+        usleep(T_KONTA);
+    else if (d == 'l')
+        usleep(T_MAKRIA);
+    else
+        fatal("Wrong input on delivery function");
 }
 
 int main() {
@@ -95,12 +104,15 @@ int main() {
     socklen_t addr_len;
     
     /* Shared memory size: order number limit + status buffers */
-    int size = LIMIT * sizeof(order_t) + 20; 
+    int size = LIMIT * sizeof(order_t) + N_DIANOMEIS + N_PSISTES; 
 
     /* ================ END OF DECLARATIONS ===================*/
 
+    /* ctrl-C signal handler */
+    signal(SIGINT, handler);
+
     /* Fork off the parent process to get into deamon mode */
-    /* UNCOMMENT THIS and line 109 TO WORK */
+    /* UNCOMMENT THIS TO WORK */
     /* pid = fork(); */
     if (pid == -1)
         fatal("Can't fork parent");
@@ -111,9 +123,9 @@ int main() {
     }
 
     /* Semaphore business */
-    sem_t *sem;
-    sem = sem_open(SEM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 1);
-    if (sem == SEM_FAILED)
+    sem_t *mutex;
+    mutex = sem_open(SEM_NAME, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR, 1);
+    if (mutex == SEM_FAILED)
         fatal("could not create semaphore");
 
     /* Shared memory allocation */
@@ -123,10 +135,9 @@ int main() {
     bool* shm_start = shmat(shm_id, NULL, 0);
     if (shm_start == (bool *)-1)
         fatal("main could not attach to shared memory");
-    /* Setting the pointers to shared memory */
-    bool* cook_status = shm_start;
-    bool* deliverer_status = shm_start + 10;
-    order_t* order_list = (order_t*)shm_start + 20;
+    /* pointer to the start of the list by shifting */
+    order_t* order_list = (order_t*)shm_start + N_DIANOMEIS + N_PSISTES;
+    /* initialization. here?? */
     for (i = 0; i < 20; i++) {
         *(shm_start + i) = false;
     }
@@ -166,20 +177,84 @@ int main() {
             break;
     }
 
-    /* Child operates below */
+    /* Childs operate below */
+
+    /* getting the pid to avoid the mess with the if-s */
+    pid = getpid();
+    /* The chosen cooker id. defaults to -1: not found */
+    int chosen = -1;
+    char pizza_type = 'n';
 	
     /* configure shared memory for every child proccess  */
     shm_id = shmget(SHM_KEY, size , 0600);
     if (shm_id == -1)          
         fatal("in shared memory");
-    /* Do we need to attach ??? */
-    /* bool* shm_start = shmat(shm_id, NULL, 0); */
-    /* if (shm_start == (bool *)-1) */
-        /* fatal("childs could not attach to shared memory"); */
+    bool* shm_begin = shmat(shm_id, NULL, 0);
+    if (shm_begin == (bool *)-1)
+        fatal("childs could not attach to shared memory");
+    /* Configuring the pointers to shared memory */
+    bool* cook_status = shm_begin;
+    bool* deliverer_status = shm_begin + N_PSISTES;
+    /* XXX: This raises error for redefinition
+     * we need to change how we are passing the data to the children */
+    order_t* order_list = (order_t*)shm_begin + N_DIANOMEIS + N_PSISTES;
+
+    /* forking for the individual pizzas */
+    while (1) {
+        while (order_list->m_num != 0) {
+            (order_list->m_num)--;
+            /* Margarita type */
+            pizza_type = 'm';
+            pid = fork();
+            if (pid == 0)
+                break;
+        }
+        while (order_list->p_num != 0) {
+            (order_list->p_num)--;
+            pizza_type = 'p';
+            pid = fork();
+            if (pid == 0)
+                break;
+        }
+        while (order_list->s_num != 0) {
+            (order_list->s_num)--;
+            pizza_type = 's';
+            pid = fork();
+            if (pid == 0)
+                break;
+        }
+        /* This is to get out of the nested while-s. Other solution: goto */
+        if (pid == 0)
+            break;
+    }
+    /* after forking for the individual pizzas, exit */
+    if (pid > 0)
+        exit(EXIT_SUCCESS);
+
+    /* FROM HERE INDIVIDUAL PIZZAS */
+    sem_wait(&mutex);
+    /* let's choose a cooker */
+    for (i = 0; i < 10; i++) {
+        if ( *(cook_status + i) == 0) {
+            *(cook_status + i) = 1;
+            chosen = i;
+            break;
+        }
+    }
+    sem_post(&mutex);
+
+    /* ready for cooking */
+    order_list->status2 = 1;
+    cook(pizza_type);
+    /* cooked */
+    order_list->status1 = 1;
+    order_list->status2 = 0;
+
+    /* THE SAME FOR DELIVERY */
 
     /* detaching from shared memory */
     if (shmdt(shm_start) == -1)
         fatal("order could not detach from shared memory");	
 	
-    /* terminate */
+    return 0;
 }
